19daa6b86d1ccd5346ace5df907691b11ee34ac7
Vale Eseve <vale.eseve@gmail.com>
2025-12-16 16:02:22 -0300

    Añadido allowed hosts a pagina de railway 2

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['https://melodiario.up.railway.app/']
+ALLOWED_HOSTS = ['melodiario.up.railway.app']
 
 
 # Application definition
